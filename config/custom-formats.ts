79db7bf3295f0cb7fe81e6ecbf90f7b0e4dcc452
JesusFreakTW <70419471+JesusFreakTW@users.noreply.github.com>
2021-10-10 00:40:24 -0400

    Added Red++
    Basic format addition for client testing. Same as Gen 1 OU with no banlist

---
+++ config/custom-formats.ts
@@ -0,0 +1,18 @@
+
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+export const Formats: FormatList = [
+
+	{
+		section: "RomHacks",
+	},
+
+	{
+		name: "[Gen 1] Red++",
+
+		mod: 'red++',
+		ruleset: ['Standard'],
+	},
+
+
+];
