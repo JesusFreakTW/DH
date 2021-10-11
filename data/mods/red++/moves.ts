5dd3f921d833a87cf78557be0566d872f29d058d
JesusFreakTW <70419471+JesusFreakTW@users.noreply.github.com>
2021-10-10 21:19:36 -0400

    Revert "Update moves.ts"
    This reverts commit 6cb93e54214fc4b301f806ebd1943310ff0ef8f5.

--- data/mods/red++/moves.ts
+++ data/mods/red++/moves.ts
@@ -536,7 +536,7 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 	karatechop: {
 		inherit: true,
 		critRatio: 2,
-		type: "Normal",
+		type: "Fighting",
 	},
 	leechseed: {
 		inherit: true,
