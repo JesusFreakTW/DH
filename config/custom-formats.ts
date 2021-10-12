84b2abae3d169ec4c3e56c218ca451db8d396f64
JesusFreakTW <70419471+JesusFreakTW@users.noreply.github.com>
2021-10-12 10:10:43 -0400

    Revert "Move red++ to formats"
    This reverts commit 013557c07a9d8e191b207ab08f1b5c87ebb7affa.

---
+++ config/custom-formats.ts
@@ -0,0 +1,18 @@
+
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+export const Formats: FormatList = [
+
+	{
+		section: "RomHacks",
+	},
+
+	{
+		name: "[Gen 1] Red++",
+
+		mod: 'redplusplus',
+		ruleset: ['Standard'],
+	},
+
+
+];
