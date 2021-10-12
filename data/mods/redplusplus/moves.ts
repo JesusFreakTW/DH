8a36474ace2acfa9c0e2884c493777b2f5ef75b0
JesusFreakTW <70419471+JesusFreakTW@users.noreply.github.com>
2021-10-11 21:32:36 -0400

    Add moves.ts and change karatechop bp

---
+++ data/mods/redplusplus/moves.ts
@@ -0,0 +1,11 @@
+/**
+ * A lot of Gen 1 moves have to be updated due to different mechanics.
+ * Some moves have had major changes, such as Bite's typing.
+ */
+
+export const Moves: {[k: string]: ModdedMoveData} = {
+	karatechop: {
+		inherit: true,
+		basePower: 55,
+	},
+};
