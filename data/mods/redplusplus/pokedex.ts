13111029c55f235099b88bd02e9e4b6f32c5c5c4
JesusFreakTW <70419471+JesusFreakTW@users.noreply.github.com>
2021-10-16 02:33:56 -0400

    Adjust existing base stats

---
+++ data/mods/redplusplus/pokedex.ts
@@ -0,0 +1,50 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	beedrill: { //atk: 80 - 90
+		inherit: true,
+		baseStats: {hp: 65, atk: 90, def: 40, spa: 45, spd: 45, spe: 75},
+	},
+	butterfree: {//spc: 80 - 90
+		inherit: true,
+		baseStats: {hp: 60, atk: 45, def: 50, spa: 90, spd: 90, spe: 70},
+	},
+	clefable: {//spc: 85 - 95
+		inherit: true,
+		baseStats: {hp: 95, atk: 70, def: 73, spa: 95, spd: 95, spe: 60},
+	},
+	golem: {//atk: 110 - 120
+		inherit: true,
+		baseStats: {hp: 80, atk: 120, def: 130, spa: 55, spd: 55, spe: 45},
+	},
+	nidoking: {//atk: 92 - 102
+		inherit: true,
+		baseStats: {hp: 81, atk: 102, def: 77, spa: 75, spd: 75, spe: 85},
+	},
+	nidoqueen: {//atk: 82 - 92
+		inherit: true,
+		baseStats: {hp: 90, atk: 92, def: 87, spa: 75, spd: 75, spe: 76},
+	},
+	pidgeot: {//spe: 91 - 101
+		inherit: true,
+		baseStats: {hp: 83, atk: 80, def: 75, spa: 70, spd: 70, spe: 101},
+	},
+	pikachu: {//def: 30 - 40
+		inherit: true,
+		baseStats: {hp: 35, atk: 55, def: 40, spa: 50, spd: 50, spe: 90},
+	},
+	poliwrath: {//atk: 85 - 95
+		inherit: true,
+		baseStats: {hp: 90, atk: 95, def: 95, spa: 70, spd: 70, spe: 70},
+	},
+	raichu: {//spe: 100 - 110
+		inherit: true,
+		baseStats: {hp: 60, atk: 90, def: 55, spa: 90, spd: 90, spe: 110},
+	},
+	vileplume: {//spc: 100 - 110
+		inherit: true,
+		baseStats: {hp: 75, atk: 80, def: 85, spa: 110, spd: 110, spe: 50},
+	},
+	wigglytuff: {//spc: 50 - 85
+		inherit: true,
+		baseStats: {hp: 140, atk: 70, def: 45, spa: 85, spd: 85, spe: 45},
+	},
+};
