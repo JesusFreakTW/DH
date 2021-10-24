ff4ec4433f3b433ee673afbded4a280ee451d5ba
JesusFreakTW <70419471+JesusFreakTW@users.noreply.github.com>
2021-10-24 01:21:33 -0400

    Test learnset script

--- data/mods/redplusplus/scripts.ts
+++ data/mods/redplusplus/scripts.ts
@@ -7,11 +7,32 @@
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen1',
 	init() {
-		const learnset = (mon: string, move: string, sources: string[]) => {
-			for (const move of moves) {
-				this.modData('Learnsets', this.toID(mon)).learnset[this.toID(move)] = sources;
+		const learnset = (learnsetChange: object) => {
+			let moveChangeArray = [];
+			let mon = "";
+			let move = "";
+			let sources = [];
+			for(let i in learnsetChange) {
+				moveChangeArray = learnsetChange[i].moveChanges;
+				mon = learnsetChange[i].name;
+				for(let j = 0; j <  moveChangeArray.length; j++) {
+					move = moveChangeArray[j][0];
+					sources = moveChangeArray[j][1];
+					this.modData('Learnsets', this.toID(mon)).learnset[this.toID(move)] = sources;
+				}
 			}
 		};
-	learnset("charmander","honeclaws",["1L16"]);
+		const learnsetChanges = {
+			charmander: {
+				name: "charmander",
+				moveChanges: [
+				["honeclaws",["1L16"]]
+				]
+			}
+		};
 	}
 };
+
+
+
+//want to be able to just write charmander one time, then write moves and levels
