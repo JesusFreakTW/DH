1138df4b4000248e75756328c2092c778acb632f
JesusFreakTW <70419471+JesusFreakTW@users.noreply.github.com>
2021-10-27 04:04:07 -0400

    Re-test Split
    Revert "Re-test Split"
    
    This reverts commit d9adb3d211b4fc540d0406cd6afdfaa6744c187a.
    
    Revert "Revert "Re-test Split""
    
    This reverts commit 6c9adeccf37931ab939f4e53be92ed23fd53d6f7.

--- data/mods/redplusplus/scripts.ts
+++ data/mods/redplusplus/scripts.ts
@@ -4,6 +4,22 @@
  * This generation inherits all the changes from older generations, that must be taken into account when editing code.
  */
 
+import { consoleips } from "../../../config/config-example";
+
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen1',
+	init() {
+		let newCategory = '';
+		for (const i in this.data.Moves) {
+			if (!this.data.Moves[i]) {
+				console.log(i);
+			}
+			if (this.data.Moves[i].category === 'Status') continue;
+			newCategory = Dex.mod('gen6').modData('Moves',i).category;
+			if (newCategory !== this.data.Moves[i].category) {
+				this.modData('Moves', i).category = newCategory;
+			}
+			newCategory = '';
+		}
+	},
 };
